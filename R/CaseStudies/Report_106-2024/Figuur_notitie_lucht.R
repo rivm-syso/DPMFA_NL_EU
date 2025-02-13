da92e9ab36808605eb29925629d3ebd1a1be63d3
Joris Quik <quikj@rivm-lsfsv-l12p.rivm.ssc-campus.nl>
2025-02-13 17:38:58 +0100

    Comment out recycling

--- R/CaseStudies/Report_106-2024/Figuur_notitie_lucht.R
+++ R/CaseStudies/Report_106-2024/Figuur_notitie_lucht.R
@@ -80,89 +80,89 @@ data_Year_sep_macro <-
 
 # Figure of only macroplastics to see contributions from which sources.
 
-############################# Prepare recycling data ###########################
+# ############################# Prepare recycling data ###########################
+# 
+# outflow_data <- 
+#   DPMFA_outflow |> unnest(Mass_Polymer_kt, keep_empty = TRUE) |> 
+#   pivot_longer(cols=-c(Type, Scale, Source, Polymer, From_compartment, To_compartment, iD_source, RUN),
+#                names_to = "Year",
+#                values_to = "Mass_Polymer_kt")
+# 
+# # Create a dataframe for just 2019
+# data_Year_outflow <- outflow_data |>
+#   filter(Year == SelectYear) |>
+#   mutate(Mass_Polymer_t = Mass_Polymer_kt * 1000, .keep = "unused") |> # conert kt to ton
+#   mutate(IenW_Source = # aggregate inputs to the Major sources classes the study focusses on
+#            case_when(
+#              str_detect(Source, 'Clothing') ~ "Textile",
+#              str_detect(Source, 'Household textiles') ~ "Textile",
+#              str_detect(Source, 'Technical textiles') ~ "Textile", 
+#              str_detect(Source, 'Import of primary plastics') ~ "Pre-production pellets",
+#              str_detect(Source, 'Domestic primary plastic production') ~ "Pre-production pellets",
+#              .default = Source
+#            ))
+# 
+# # # Sum over polymers
+# # outflow_all_mat <- data_Year_outflow |>
+# #   group_by(Type, Scale, IenW_Source, Source, From_compartment, To_compartment, Year, RUN) |>
+# #   summarise(Mass_Polymer_t = sum(Mass_Polymer_t))
+# 
+# # Get the rows that contain recycling in the from compartment, all to_compartments coming from recycling are sinks
+# Recycling_data_subset <- data_Year_outflow |>
+#   filter(grepl("recycling", From_compartment)) |>
+#   mutate(IenW_Source = "Pre-production pellets")
+# 
+# # Group the recycling data together (so from comp is recycling, to is environmental compartments)
+# Recycling_data_grouped <- Recycling_data_subset |>
+#   group_by(Type, Scale, To_compartment, Year, RUN) |>
+#   summarise(Mass_Polymer_t = sum(Mass_Polymer_t)) |>
+#   mutate(From_compartment = "Recycling") 
+# 
+# # Make a table to save, by calculating the mean over runs
+# table <- Recycling_data_subset |>
+#   group_by(Type, Scale, From_compartment, To_compartment,Year) |>
+#   summarise(Mass_Polymer_t = mean(Mass_Polymer_t))
+# 
+# Recycling <- data_Year_outflow |>
+#   filter(grepl("recycling", To_compartment)) |>
+#   group_by(Type, Scale, From_compartment, To_compartment, Year, RUN) |>
+#   summarise(Mass_Polymer_t = sum(Mass_Polymer_t)) |> # sum over polymers for table
+#   ungroup() |>
+#   group_by(Type, Scale, From_compartment, To_compartment,Year) |> # sum over runs for table
+#   summarise(Mass_Polymer_t = mean(Mass_Polymer_t))
+# 
+# Recycling <-rbind(table, Recycling)
 
-outflow_data <- 
-  DPMFA_outflow |> unnest(Mass_Polymer_kt, keep_empty = TRUE) |> 
-  pivot_longer(cols=-c(Type, Scale, Source, Polymer, From_compartment, To_compartment, iD_source, RUN),
-               names_to = "Year",
-               values_to = "Mass_Polymer_kt")
-
-# Create a dataframe for just 2019
-data_Year_outflow <- outflow_data |>
-  filter(Year == SelectYear) |>
-  mutate(Mass_Polymer_t = Mass_Polymer_kt * 1000, .keep = "unused") |> # conert kt to ton
-  mutate(IenW_Source = # aggregate inputs to the Major sources classes the study focusses on
-           case_when(
-             str_detect(Source, 'Clothing') ~ "Textile",
-             str_detect(Source, 'Household textiles') ~ "Textile",
-             str_detect(Source, 'Technical textiles') ~ "Textile", 
-             str_detect(Source, 'Import of primary plastics') ~ "Pre-production pellets",
-             str_detect(Source, 'Domestic primary plastic production') ~ "Pre-production pellets",
-             .default = Source
-           ))
-
-# # Sum over polymers
-# outflow_all_mat <- data_Year_outflow |>
-#   group_by(Type, Scale, IenW_Source, Source, From_compartment, To_compartment, Year, RUN) |>
-#   summarise(Mass_Polymer_t = sum(Mass_Polymer_t))
-
-# Get the rows that contain recycling in the from compartment, all to_compartments coming from recycling are sinks
-Recycling_data_subset <- data_Year_outflow |>
-  filter(grepl("recycling", From_compartment)) |>
-  mutate(IenW_Source = "Pre-production pellets")
-
-# Group the recycling data together (so from comp is recycling, to is environmental compartments)
-Recycling_data_grouped <- Recycling_data_subset |>
-  group_by(Type, Scale, To_compartment, Year, RUN) |>
-  summarise(Mass_Polymer_t = sum(Mass_Polymer_t)) |>
-  mutate(From_compartment = "Recycling") 
-
-# Make a table to save, by calculating the mean over runs
-table <- Recycling_data_subset |>
-  group_by(Type, Scale, From_compartment, To_compartment,Year) |>
-  summarise(Mass_Polymer_t = mean(Mass_Polymer_t))
-
-Recycling <- data_Year_outflow |>
-  filter(grepl("recycling", To_compartment)) |>
-  group_by(Type, Scale, From_compartment, To_compartment, Year, RUN) |>
-  summarise(Mass_Polymer_t = sum(Mass_Polymer_t)) |> # sum over polymers for table
-  ungroup() |>
-  group_by(Type, Scale, From_compartment, To_compartment,Year) |> # sum over runs for table
-  summarise(Mass_Polymer_t = mean(Mass_Polymer_t))
-
-Recycling <-rbind(table, Recycling)
-
-write.xlsx(Recycling, paste0(data_folder, "/Output_tables/Recycling_", ModelRun, ".xlsx"))
+# write.xlsx(Recycling, paste0(data_folder, "/Output_tables/Recycling_", ModelRun, ".xlsx"))
 
 # Get other data, without the recycling data
-non_recycling_data <- data_Year_outflow |>
-  filter(!grepl("recycling", From_compartment)) 
-
-# Make a df where the recycling emissions are attributed to pre-production pellets
-#Recycling_data <- rbind(Recycling_data_grouped, non_recycling_data)
-
-Recycling_data <- rbind(Recycling_data_subset, non_recycling_data)
-
-# Get only the sink compartments
-Recycling_data <- Recycling_data |>
-  filter(To_compartment %in% unique(DPMFA_sink$To_Compartment)) |>
-  mutate(`Environmental_compartment` = # make column indicating of this an air, water or soil sink.
-           case_when(
-             str_detect(To_compartment, 'water') ~ "water",
-             str_detect(To_compartment, 'air') ~ "air",
-             str_detect(To_compartment, 'soil') ~ "soil",
-             .default = "none"
-           )) |> filter(`Environmental_compartment` != "none") |> # remove sinks that are not environmental 
-  separate_wider_delim(cols = To_compartment,
-                       names = c("To_compartment", "Material_Type"),
-                       delim = " (",
-                       too_few = "align_start",
-                       too_many = "merge",
-                       cols_remove = FALSE) |> 
-  mutate(Material_Type = str_remove(Material_Type , "\\)")) |>
-  mutate(Material_Type = case_when(Material_Type %in% c('macro', 'micro') ~ as.character(Material_Type),
-                                   TRUE ~ NA_character_))
+# non_recycling_data <- data_Year_outflow |>
+#   filter(!grepl("recycling", From_compartment)) 
+# 
+# # Make a df where the recycling emissions are attributed to pre-production pellets
+# #Recycling_data <- rbind(Recycling_data_grouped, non_recycling_data)
+# 
+# Recycling_data <- rbind(Recycling_data_subset, non_recycling_data)
+# 
+# # Get only the sink compartments
+# Recycling_data <- Recycling_data |>
+#   filter(To_compartment %in% unique(DPMFA_sink$To_Compartment)) |>
+#   mutate(`Environmental_compartment` = # make column indicating of this an air, water or soil sink.
+#            case_when(
+#              str_detect(To_compartment, 'water') ~ "water",
+#              str_detect(To_compartment, 'air') ~ "air",
+#              str_detect(To_compartment, 'soil') ~ "soil",
+#              .default = "none"
+#            )) |> filter(`Environmental_compartment` != "none") |> # remove sinks that are not environmental 
+#   separate_wider_delim(cols = To_compartment,
+#                        names = c("To_compartment", "Material_Type"),
+#                        delim = " (",
+#                        too_few = "align_start",
+#                        too_many = "merge",
+#                        cols_remove = FALSE) |> 
+#   mutate(Material_Type = str_remove(Material_Type , "\\)")) |>
+#   mutate(Material_Type = case_when(Material_Type %in% c('macro', 'micro') ~ as.character(Material_Type),
+#                                    TRUE ~ NA_character_))
 
 ################################### Figures ####################################
 # Create a theme for the plots
